5bfb137e69256f25bd3a4d0320c45506e84d07cd
Daniel Wijaya <dwjy27@gmail.com>
2022-02-23 20:24:33 +0700

    refactor code

--- js/figure.js
+++ js/figure.js
@@ -1,44 +1,43 @@
 import { generateCard, getData, Type } from './util.js';
 
 const figures = await getData(Type.Figures);
-
-$('.card-container').empty();
-
-figures.forEach((figure) => {
-    $('.card-container').append(generateCard(figure));
-});
-
-//Search
+const container = $('.card-container');
 $('.search-closeicon').css({ display: 'none', cursor: 'pointer' });
 
-$('.search-closeicon').click((e) => {
+// Utility function for merch
+function generateResults(query = null) {
+    const needFilter = !query;
+    container
+        .empty()
+        .append(
+            figures
+                .filter((figure) => needFilter || figure.name.toLowerCase().includes(query.toLowerCase()))
+                .map((figure) => generateCard(figure))
+        );
+}
+
+function resetSearchbarIcon() {
     $('#searchbar').val('');
     $('.search-closeicon').hide();
     $('.search-searchicon').show();
+}
 
-    $('.card-container').empty();
-    figures.forEach((figure) => {
-        $('.card-container').append(generateCard(figure));
-    });
+// Default action
+generateResults();
+
+$('.search-closeicon').on('click', (e) => {
+    resetSearchbarIcon();
+    generateResults();
 });
 
 $('#searchbar').on('input', function (e) {
-    if ($(e.target).val() != '') {
+    const query = $(e.target).val();
+    if (query !== '') {
         $('.search-closeicon').show();
         $('.search-searchicon').hide();
     } else {
         $('.search-closeicon').hide();
         $('.search-searchicon').show();
     }
-
-    //filter
-    // ubah input ke lowercase
-    // loop jsonnya -> if include di search -> generate card
-    $('.card-container').empty();
-    let search_object = $(e.target).val().toLowerCase();
-    figures.forEach((figure) => {
-        if (figure.name.toLowerCase().includes(search_object)) {
-            $('.card-container').append(generateCard(figure));
-        }
-    });
+    generateResults(query);
 });
